581ef483adf73f850f81daa28065f1d530819adf
Akshay Yadav BR <akshayyadavbr@gmail.com>
2025-04-02 10:33:17 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", {id:"parent"}, 
+    [React.createElement("div", {id:"child"},
+        [React.createElement("h1", {},"im h1 tag"),
+        React.createElement("h2", {},"im h2 tag")]),
+     React.createElement("div", {id:"child2"},
+        [React.createElement("h1", {},"im h1 tag"),
+        React.createElement("h2", {},"im h2 tag")])   
+    ]);
+
+
+
+const heading = React.createElement("h1", {id:"container"}, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
